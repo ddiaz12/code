e9f70b3b44f797fcd37d75ad26941f80a1766420
MisaelLG1999 <132076363+MisaelLG1999@users.noreply.github.com>
2024-05-16 12:58:59 -0600

    se agrego login y regulaciones

---
+++ home/home-sujeto.blade.php
@@ -0,0 +1,64 @@
+@include('templates/header')
+<body class="sb-nav-fixed cuerpo-sujeto">
+
+    <!-- Navbar -->
+    @include('templates/navbar')
+    <!-- Navbar -->
+
+    <div id="layoutSidenav">
+        
+        <!-- Menu -->
+        @include('templates/menu')
+        <!-- Menu -->
+
+        <!-- Contenido -->
+        <div id="layoutSidenav_content" class="div-contenido">
+            <main class="main-contenido">
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
+
+                    <div class="row">
+                        <div class="col-sm-6 mb-3 mb-sm-0 div-card">
+                            <div class="card h-100 d-flex align-items-center card-regulacion">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">Regulaciones</h5>
+                                    <p class="card-text text-regulacion">Administra cualquier normativa de caracter
+                                        general.</p>
+                                    <a href="http://localhost/code-main/home/regulaciones" class="btn btn-primary btn-regulacion">administrar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 div-card">
+                            <div class="card h-100 d-flex align-items-center card-oficina">
+                                <div class="card-body card-oficinas text-center">
+                                    <h5 class="card-title">Oficinas</h5>
+                                    <p class="card-text">Administrar usuarios de la plataforma del Catálogo Nacional de
+                                        Trámites y Servicios.</p>
+                                    <a href="<?php echo base_url("oficinas/oficina") ?>"
+                                        class="btn btn-primary btn-oficina">administrar</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 div-card div-usuarios">
+                            <div class="card h-100 d-flex align-items-center card-oficina">
+                                <div class="card-body card-usuario text-center">
+                                    <h5 class="card-title">Usuarios</h5>
+                                    <p class="card-text">Administrar usuarios de la plataforma  del Catalogo Nacional 
+                                        de Tramites y Servicios.</p>
+                                    <a href="<?php echo base_url("usuarios/usuario") ?>"
+                                        class="btn btn-primary btn-oficina">administrar</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+
+            <!-- Footer -->
+            @include('templates/footer')
+            <!-- Footer -->
+        </div>
+        <!-- Contenido -->
+    </div>
+</body>
---
+++ sujeto/usuarios.blade.php
@@ -0,0 +1,109 @@
+@include('templates/header')
+
+<body class="sb-nav-fixed cuerpo-sujeto">
+
+    <!-- Navbar -->
+    @include('templates/navbar')
+    <!-- Navbar -->
+
+    <div id="layoutSidenav">
+
+        <!-- Menu -->
+        @include('templates/menu')
+        <!-- Menu -->
+
+        <!-- Contenido -->
+        <div id="layoutSidenav_content" class="div-contenido">
+            <main>
+                <div class="container-fluid px-4">
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item"><a href="<?php echo base_url('home/home_sujeto'); ?>"><i class="fas fa-home me-1"></i>Home</a>
+                        </li>
+                        <li class="breadcrumb-item active"><i class="fas fa-users me-1"></i>Usuarios</li>
+                    </ol>
+                    <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
+                    <!-- Botón para abrir otra vista -->
+                    <div class="d-flex justify-content-end mb-3">
+                        <a href="<?php echo base_url('Usuarios/agregar_usuario'); ?>" class="btn btn-primary btn-agregarUsuario">
+                            <i class="fas fa-plus-circle me-1"></i> Agregar Usuario
+                        </a>
+                    </div>
+
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <table id="datatablesSimple">
+                                <thead>
+                                    <tr>
+                                        <th class="tTabla-color">Id</th>
+                                        <th class="tTabla-color">Nombre completo</th>
+                                        <th class="tTabla-color">Correo electronico</th>
+                                        <th class="tTabla-color">Fecha</th>
+                                        <th class="tTabla-color">Numero de telefono</th>
+                                        <th class="tTabla-color">Rol</th>
+                                        <th class="tTabla-color">Estatus</th>
+                                        <th class="tTabla-color">Acciones </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+                                    @foreach ($usuarios as $usuario)
+                                        <tr>
+                                            <td>{{ $usuario->ID_Usuario }}</td>
+                                            <td>{{ $usuario->Nombre }} {{ $usuario->Apellido_Paterno }}
+                                                {{ $usuario->Apellido_Materno }}</td>
+                                            <td>{{ $usuario->Correo_Electronico }}</td>
+                                            <td>{{ $usuario->Fecha_Cargo_ROM }}</td>
+                                            <td>{{ $usuario->Num_Tel }}</td>
+                                            <td>{{ $usuario->Roles }}</td>
+                                            <td>{{ $usuario->Estatus == 1 ? 'Activo' : 'Inactivo' }}</td>
+                                            <td>
+                                                <a href="<?php echo base_url('usuarios/editar/' . $usuario->ID_Usuario); ?>" class="btn btn-warning">
+                                                    <i class="fas fa-edit"></i>
+                                                </a>
+                                                <button type="button" class="btn btn-danger"
+                                                    data-id_usuario="<?php echo $usuario->ID_Usuario; ?>"><i
+                                                        class="fas fa-trash"></i></button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <!-- Footer -->
+            @include('templates/footer')
+            <!-- Footer -->
+        </div>
+        <!-- Contenido -->
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            $('#datatablesSimple').DataTable({
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+                }
+            });
+        });
+
+        $(document).ready(function() {
+            $('.btn-danger').click(function() {
+                var id = $(this).data('id_usuario');
+                $.ajax({
+                    url: '<?php echo base_url('usuarios/eliminar/') ?>' + id,
+                    type: 'GET',
+                    success: function (result) {
+                        // Recargar la página o hacer algo con el resultado
+                        location.reload();
+                    }
+                });
+            });
+        });
+    </script>
+
+</body>
