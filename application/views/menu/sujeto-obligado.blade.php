cfb3d114ff101ab28e6357ab740b02650bbb0c41
ddiaz12 <rdiazmorfin@gmail.com>
2024-05-27 13:02:07 -0600

    Acomodo de plantillas de sujeto obligado

--- application/views/menu/sujeto-obligado.blade.php
+++ application/views/menu/sujeto-obligado.blade.php
@@ -1,81 +1,68 @@
-@include('templates/header')
+@layout('templates/master')
+@section('titulo')
+    Registro Estatal de Regulaciones
+@endsection
+@section('menu')
+    @include('templates/menuSujeto')
+@endsection
+@section('contenido')
+    <!-- Contenido -->
+    <div class="container-fluid px-4">
 
-<body class="sb-nav-fixed cuerpo-sujeto">
-
-    <!-- Navbar -->
-    @include('templates/navbar')
-    <!-- Navbar -->
-
-    <div id="layoutSidenav">
-
-        <!-- Menu -->
-        @include('templates/menu')
-        <!-- Menu -->
-
-        <!-- Contenido -->
-        <div id="layoutSidenav_content" class="div-img">
-            <main>
-                <div class="container-fluid px-4">
-
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item"><a href="<?php echo base_url('home/home_sujeto'); ?>"><i class="fas fa-home me-1"></i>Home</a>
-                        </li>
-                        <li class="breadcrumb-item active"><i class="fas fa-user me-1"></i>Sujeto obligado</li>
-                    </ol>
-                    <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
-                    <!-- Botón para abrir otra vista -->
-                    <div class="d-flex justify-content-end mb-3">
-                        <a href="<?php echo base_url('menu/agregar_sujeto'); ?>" class="btn btn-primary btn-agregarOficina">
-                            <i class="fas fa-plus-circle me-1"></i> Agregar sujeto obligado
-                        </a>
-                    </div>
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <table id="datatablesSimple">
-                                <thead>
-                                    <tr>
-                                        <th class="tTabla-color">Id</th>
-                                        <th class="tTabla-color">Nombre sujeto obligado</th>
-                                        <th class="tTabla-color">Siglas</th>
-                                        <th class="tTabla-color">Tipo</th>
-                                        <th class="tTabla-color">Materia</th>
-                                        <th class="tTabla-color">Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($sujetos as $sujeto): ?>
-                                        <tr>
-                                            <td><?php echo $sujeto->ID_sujeto; ?></td>
-                                            <td><?php echo $sujeto->nombre_sujeto; ?></td>
-                                            <td><?php echo $sujeto->siglas; ?></td>
-                                            <td><?php echo $sujeto->tipo_sujeto; ?></td>
-                                            <td><?php echo $sujeto->materia; ?></td>
-                                            <td>
-                                                <a href="<?php echo base_url('menu/editar_sujeto/' . $sujeto->ID_sujeto); ?>"
-                                                    class="btn btn-warning btn-sm">
-                                                    <i class="fas fa-edit" title="Editar sujeto"></i>
-                                                </a>
-                                                <button class="btn btn-danger btn-sm"
-                                                    data-id_sujeto="<?php echo $sujeto->ID_sujeto; ?>">
-                                                    <i class="fas fa-trash" title="Eliminar sujeto"></i>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </main>
-
-            <!-- Footer -->
-            @include('templates/footer')
-            <!-- Footer -->
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="<?php echo base_url('home/home_sujeto'); ?>"><i class="fas fa-home me-1"></i>Home</a>
+            </li>
+            <li class="breadcrumb-item active"><i class="fas fa-user me-1"></i>Sujeto obligado</li>
+        </ol>
+        <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
+        <!-- Botón para abrir otra vista -->
+        <div class="d-flex justify-content-end mb-3">
+            <a href="<?php echo base_url('menu/agregar_sujeto'); ?>" class="btn btn-primary btn-agregarOficina">
+                <i class="fas fa-plus-circle me-1"></i> Agregar sujeto obligado
+            </a>
         </div>
-        <!-- Contenido -->
+        <div class="card mb-4">
+            <div class="card-body">
+                <table id="datatablesSimple">
+                    <thead>
+                        <tr>
+                            <th class="tTabla-color">Id</th>
+                            <th class="tTabla-color">Nombre sujeto obligado</th>
+                            <th class="tTabla-color">Siglas</th>
+                            <th class="tTabla-color">Tipo</th>
+                            <th class="tTabla-color">Materia</th>
+                            <th class="tTabla-color">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($sujetos as $sujeto): ?>
+                        <tr>
+                            <td><?php echo $sujeto->ID_sujeto; ?></td>
+                            <td><?php echo $sujeto->nombre_sujeto; ?></td>
+                            <td><?php echo $sujeto->siglas; ?></td>
+                            <td><?php echo $sujeto->tipo_sujeto; ?></td>
+                            <td><?php echo $sujeto->materia; ?></td>
+                            <td>
+                                <a href="<?php echo base_url('menu/editar_sujeto/' . $sujeto->ID_sujeto); ?>" class="btn btn-warning btn-sm">
+                                    <i class="fas fa-edit" title="Editar sujeto"></i>
+                                </a>
+                                <button class="btn btn-danger btn-sm" data-id_sujeto="<?php echo $sujeto->ID_sujeto; ?>">
+                                    <i class="fas fa-trash" title="Eliminar sujeto"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-
+    <!-- Contenido -->
+@endsection
+@section('footer')
+    @include('templates/footer')
+@endsection
+@section('js')
     <script>
         $(document).ready(function() {
             $('#datatablesSimple').DataTable({
@@ -127,5 +114,4 @@
             });
         });
     </script>
-
-</body>
+@endsection
