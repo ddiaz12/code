093db60ac22418216f5622f26cc01d0c9f004536
ddiaz12 <rdiazmorfin@gmail.com>
2024-05-27 00:02:54 -0600

    Estructura master

--- application/views/home/home-sujeto.blade.php
+++ application/views/home/home-sujeto.blade.php
@@ -1,58 +1,38 @@
-@include('templates/header')
+@layout('templates/master')
+@section('titulo')
+    Registro Estatal de Regulaciones
+@endsection
 
-<body class="sb-nav-fixed cuerpo-sujeto">
-
-    <!-- Navbar -->
-    @include('templates/navbar')
-    <!-- Navbar -->
-
-    <div id="layoutSidenav">
-
-        <!-- Menu -->
-        @include('templates/menu')
-        <!-- Menu -->
-
-        <!-- Contenido -->
-        <div id="layoutSidenav_content" class="div-img">
-
-            <main class="main-contenido">
-                <div class="container-fluid px-4 ">
-                    <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
-
-                    <div class="row no-padding">
-                        <div class="card shadow mb-4 col-sm-5 div-oficinas">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-cards">Oficinas</h6>
-                            </div>
-                            <div class="card-body ">
-                                Administrar usuarios de la plataforma del Catálogo Nacional de
-                                Trámites y Servicios.
-                            </div>
-                            <div class="card-footer text-center">
-                                <a href="<?php echo base_url('oficinas'); ?>" class="btn btn-primary btn-oficina">administrar</a>
-                            </div>
-                        </div>
-                        <div class="card shadow mb-4 col-sm-5 div-usuarios">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-cards">Usuarios</h6>
-                            </div>
-                            <div class="card-body">
-                                Administrar usuarios de la plataforma del Catalogo Nacional
-                                de Tramites y Servicios.
-                            </div>
-                            <div class="card-footer text-center">
-                                <a href="<?php echo base_url('usuarios/usuario'); ?>" class="btn btn-primary btn-oficina">administrar</a>
-                            </div>
-                        </div>
-                    </div>
-
+@section('contenido')
+    <!-- Contenido -->
+    <div class="container-fluid px-4 ">
+        <h1 class="mt-4 titulo-menu">Registro Estatal de Regulaciones (RER)</h1>
+        <div class="row no-padding">
+            <div class="card shadow mb-4 col-sm-5 div-oficinas">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-cards">Oficinas</h6>
                 </div>
-            </main>
-
-            <!-- Footer -->
-            @include('templates/footer')
-            <!-- Footer -->
+                <div class="card-body ">
+                    Administrar usuarios de la plataforma del Catálogo Nacional de
+                    Trámites y Servicios.
+                </div>
+                <div class="card-footer text-center">
+                    <a href="<?php echo base_url('oficinas'); ?>" class="btn btn-primary btn-oficina">administrar</a>
+                </div>
+            </div>
+            <div class="card shadow mb-4 col-sm-5 div-usuarios">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-cards">Usuarios</h6>
+                </div>
+                <div class="card-body">
+                    Administrar usuarios de la plataforma del Catalogo Nacional
+                    de Tramites y Servicios.
+                </div>
+                <div class="card-footer text-center">
+                    <a href="<?php echo base_url('usuarios/usuario'); ?>" class="btn btn-primary btn-oficina">administrar</a>
+                </div>
+            </div>
         </div>
-        <!-- Contenido -->
     </div>
-</body>
+    <!-- Contenido -->
+@endsection
--- application/views/templates/master.blade.php
+++ application/views/templates/master.blade.php
@@ -8,56 +8,44 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>@yield('titulo')</title>
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    <!-- Aquí cargamos los estilos CSS -->
+    @yield('css')
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css">
+    <link href= "<?php echo site_url('assets/css/styles.css'); ?>" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo site_url('assets/css/base.css'); ?>">
+
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
-    <script src="{{ base_url('assets/') }}js/scripts.js"></script>
-
-    <!-- Aquí cargamos los estilos CSS -->
-    @yield('estilos')
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="{{ base_url('assets/') }}css/styles.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ base_url('assets/') }}css/base.css">
-
+    <script src="<?php echo base_url('assets/'); ?>js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body class="sb-nav-fixed cuerpo-sujeto">
-
     <!-- Navbar -->
     @include('templates/navbar')
     <!-- Navbar -->
-
     <div id="layoutSidenav">
-
         <!-- Menu -->
         @include('templates/menu')
         <!-- Menu -->
-
         <!-- Contenido -->
-        <div id="layoutSidenav_content" class="div-contenido">
+        <div id="layoutSidenav_content" class="div-img">
             <main class="main-contenido">
-                <div class="container-fluid px-4">
-                    <h1 class="mt-4 titulo-menu">@yield('titulo_menu')</h1>
-
-                    <div class="row">
-                        @yield('contenido')
-                    </div>
-
-                </div>
+                @yield('contenido')
             </main>
-
             <!-- Footer -->
             @include('templates/footer')
             <!-- Footer -->
         </div>
         <!-- Contenido -->
     </div>
-
-    @yield('scripts')
-
+    <!-- Aquí cargamos los scripts JS -->
+    @yield('js')
 </body>
 
 </html>
