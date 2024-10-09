bbf811380d28cb43cdce1dce6ff61f74314cfea4
ddiaz12 <rdiazmorfin@gmail.com>
2024-10-09 13:23:37 -0600

    Footer de ciudadania mas pequeño

--- application/views/templates/footerCiudadania.blade.php
+++ application/views/templates/footerCiudadania.blade.php
@@ -5,9 +5,9 @@
             <div class="row">
                 <div class="col-md-6 col-12 container-img-footer">
                     <div class="row">
-                        <div class="col-md-12 col-12 ">
+                        <div class="col-md-12 col-12">
                             <img class="img-fluid" src="https://openapis.col.gob.mx/API_PU/img/logo-colima-2021.png"
-                                alt="Colima">
+                                alt="Colima" style="max-width: 150px;">
                         </div>
                         <div class="col-md-12 col-12 hidden-sm-down">
                             <p class="footer-texto-pu ">Complejo Administrativo del Gobierno del Estado 3er. Anillo
@@ -21,7 +21,6 @@
                                 href="http://www.col.gob.mx/Portal/contenido/MTA3MzI="
                                 title="Pol&iacute;ticas de uso">Pol&iacute;ticas de uso y privacidad</a>
                             <!-- http://www.col.gob.mx/portal/contenido/OTQ5MA== -->
-
                         </div>
                     </div>
                 </div>
@@ -31,44 +30,61 @@
                             <ul class="list-inline">
                                 <li class="list-inline-item"><span class="footer-icon-separador-pu"></span><a
                                         href="https://www.facebook.com/gobiernocolima/" target="_blank"
-                                        title="Facebook"><i class="fa fa-facebook-official footer-facebook fa-3x"
-                                            aria-hidden="true"></a></i></li>
+                                        title="Facebook"><i class="fa fa-facebook-official footer-facebook fa-2x"
+                                            aria-hidden="true"></a></i></li> <!-- Cambiado de fa-3x a fa-2x -->
                                 <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a
                                         href="https://twitter.com/gobiernocolima" target="_blank" title="Twitter"><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 512 512">
                                             <path
                                                 d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
                                         </svg></a></i></li>
                                 <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a
                                         href="https://www.youtube.com/user/GobiernoColima" target="_blank"
-                                        title="Youtube"><i class="fa fa-youtube-play footer-youtube fa-3x"
+                                        title="Youtube"><i class="fa fa-youtube-play footer-youtube fa-2x"
                                             aria-hidden="true"></a></i></li>
-                                <!-- <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a href="http://gobiernocolima.blogspot.mx/" target="_blank" title="Blogspot"><i class="fa fa-rss-square footer-rss fa-3x" aria-hidden="true"></a></i></li> -->
-                                <!-- <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a href="https://soundcloud.com/gobcolradio/" target="_blank" title="SoundCloud"><i class="fa fa-soundcloud footer-soundcloud fa-3x" aria-hidden="true"></a></i></li> -->
+                                <!--
+                                <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a
+                                        href="http://gobiernocolima.blogspot.mx/" target="_blank" title="Blogspot"><i
+                                            class="fa fa-rss-square footer-rss fa-2x" aria-hidden="true"></a></i></li>
+                                -->
+                                <li class="list-inline-item "><span class="footer-icon-separador-pu"></span><a
+                                        href="https://soundcloud.com/gobcolradio/" target="_blank" title="SoundCloud"><i
+                                            class="fa fa-soundcloud footer-soundcloud fa-2x" aria-hidden="true"></a></i>
+                                </li>
                             </ul>
                         </div>
                         <div class="col-12">
                             <div class="row padding-left-30">
                                 <div class="col text-center">
-                                    <i class="fa fa-download fa-2x"></i><br>
+                                    <i class="fa fa-download fa-1x"></i><br> <!-- Cambiado de fa-2x a fa-1x -->
                                     <a href="http://www.col.gob.mx/Portal/intranet" target="_blank" title="Intranet"
-                                        class="linksfooter">Intranet</a>
-                                </div>
-                                <!-- <div class="col text-center">
-                        		 <i class="fa fa-qrcode fa-2x" aria-hidden="true"></i><br>
-			   			 	  		<a href="http://www.firel.col.gob.mx/" target="_blank" title="Validación e.firma SAT" class="linksfooter">Validaci&oacute;n <br>e.firma SAT</a> 
-                        	</div> -->
-                                <div class="col text-center">
-                                    <i class="fa fa-user fa-2x" aria-hidden="true"></i><br>
-                                    <a href="http://directoriointegral.col.gob.mx/" target="_blank"
-                                        title="Directorio integral" class="linksfooter">Directorio <br> integral</a>
+                                        class="linksfooter" style="font-size: 0.8rem;">Intranet</a>
+                                    <!-- Reducir tamaño del texto -->
                                 </div>
                                 <div class="col text-center">
-                                    <i class="fa fa-check-circle-o fa-2x" aria-hidden="true"></i><br>
+                                    <i class="fa fa-qrcode fa-1x" aria-hidden="true"></i><br>
+                                    <!-- Cambiado de fa-2x a fa-1x -->
                                     <a href="http://www.firel.col.gob.mx/" target="_blank"
-                                        title="Validación de documentos con firma electrónica"
-                                        class="linksfooter">Validaci&oacute;n &nbsp; <br> de documentos</a>
+                                        title="Validación e.firma SAT" class="linksfooter"
+                                        style="font-size: 0.8rem;">Validaci&oacute;n <br>e.firma SAT</a>
+                                    <!-- Reducir tamaño del texto -->
+                                </div>
+
+                                <div class="col text-center">
+                                    <i class="fa fa-user fa-1x" aria-hidden="true"></i><br>
+                                    <a href="http://directoriointegral.col.gob.mx/" target="_blank"
+                                        title="Directorio integral" class="linksfooter"
+                                        style="font-size: 0.8rem;">Directorio <br> integral</a>
+                                </div>
+
+                                <div class="col text-center">
+                                    <i class="fa fa-check-circle-o fa-1x" aria-hidden="true"></i><br>
+                                    <!-- Cambiado de fa-2x a fa-1x -->
+                                    <a href="http://www.firel.col.gob.mx/" target="_blank"
+                                        title="Validación de documentos con firma electrónica" class="linksfooter"
+                                        style="font-size: 0.8rem;">Validaci&oacute;n &nbsp; <br> de documentos</a>
+                                    <!-- Reducir tamaño del texto -->
                                 </div>
                             </div>
                         </div>
